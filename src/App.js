0bca66e691183e52140748fb88333bb4be5b56f4
Q3421Serhat <extoche.398896@gmail.com>
2023-08-07 14:59:52 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import React from 'react'
 const App = () => {
   return (
     <div>
+      <Navbar />
 <Register />
     </div>
   );
