def830018bc86ad786724a2d3a1973b0f42c8c38
Q3421Serhat <extoche.398896@gmail.com>
2023-08-08 21:42:45 +0200

    added

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,17 @@
 import React, { createContext, useContext } from 'react'
 import { auth } from '../auth/firebase'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { Navigate, useNavigate } from 'react-router-dom';
 export const AuthContext = createContext()
+
+
 
 // export const useAuthContext =() => {
 //     return useContext(AuthContext)
 // }
 
 const AuthContextProvider  = ({children}) => {
+    let navigate = useNavigate();
 const createUser = async(email, password) => {
     try {
         let userCredential = await createUserWithEmailAndPassword(
@@ -15,8 +19,9 @@ const createUser = async(email, password) => {
             email, 
             password
         );
+        navigate('/');
             console.log(userCredential); 
-        
+      
     } catch (error) {
         console.log(error.message);  
     }
@@ -24,7 +29,13 @@ const createUser = async(email, password) => {
 
 const signIn = async(email, password) => {
     try {
-        await signInWithEmailAndPassword(auth, email, password)
+       let userCredential = await signInWithEmailAndPassword(
+        auth, 
+        email, 
+        password
+        );
+        navigate('/');
+       console.log(userCredential)
     } catch (error) {
       console.log(error.message)  
     }
@@ -34,8 +45,11 @@ const signIn = async(email, password) => {
  const values = {createUser, signIn};
 
 
-  return <AuthContext.Provider value={values}>{children} </AuthContext.Provider>
+  return (
+  <AuthContext.Provider value={values}>
+    {children} 
+    </AuthContext.Provider>
   
-}
+)};
 
 export default AuthContextProvider; 
